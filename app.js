c6147b4f0a316f40cd31b394217d8817e3175d45
NightenDushi <nathan.guilhot@gmx.fr>
2023-10-23 14:41:40 +0200

    remove unused lines

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 const http = require('http');
 const path = require('path');
 const fs = require('fs');
-// const auth = require('./user_auth');
 
 const express = require('express');
 const contact_router = require('./routes/contact')
-const user_router = require('./routes/user')
 const upload_router = require('./routes/upload')
 const proxy_router = require('./routes/proxy')
 const app = express();
@@ -21,16 +19,9 @@ app.use((req, res, next)=>{
     next();
 })
 
-app.use(express.json({limit: '1mb'})); // json -> req.body
-// app.use(express.urlencoded( {extended: true} )); //turn post into json data
-// app.use(express.static("views")); //folder to serve static files
-// app.use()
-// app.get('/', (req, res)=>{
-//     res.send("Hello!");
-// });
+app.use(express.json({limit: '1mb'}));
 
 app.use("/api/contacts", contact_router);
-app.use("/api/users", user_router);
 app.use("/api/upload", upload_router);
 app.use("/api/proxy", proxy_router);
 
@@ -65,23 +56,3 @@ namenook_app.listen(namenook_PORT, () => {
     console.log(`Namenook served on port: ${namenook_PORT}`)
     console.log(namenook_path)
 });
-
-// app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
-// express.static('public')
-// http
-//   .createServer((req, res) => {
-//     fs.readFile(path.join(__dirname,'..','/namenook/dist/index.html'), 'utf-8', (err, content) => {
-//       if (err) {
-//         console.log('We cannot open "index.html" file.')
-//       }
-
-//       res.writeHead(200, {
-//         'Content-Type': 'text/html; charset=utf-8',
-//       })
-
-//       res.end(content)
-//     })
-//   })
-//   .listen(3000, () => {
-//     console.log('   Namenook served on: http://localhost:%s', 3000)
-//   })
--- routes/upload.js
+++ routes/upload.js
@@ -5,17 +5,13 @@ const fs = require('fs');
 var crypto = require('crypto');
 const path = require('path');
 
-//TODO(Nighten) Replace this by a config and env variable
 const PATH_TO_IMAGE_FOLDER = process.env.NAMENOOK_PATH_IMAGE_FOLDER || "C:/Users/Lenovo/code/NODEJS/namenook/src/assets/profile";
-// console.log("PATH_TO_IMAGE_FOLDER:",PATH_TO_IMAGE_FOLDER)
-// console.log("ENV:",process.env)
+
 upload_router.post('/:id', async (req, res)=>{
     if (!req.body.data) return res.status(300).send("Bad input");
     const filetype = req.body.type.split("/")[1]
-    // console.log(req.body.data)
-    // req.params.id
+
     const owner_id = req.query.owner_id;
-    // console.log(req.query)
     if (!owner_id || owner_id=="undefined" || owner_id==undefined) {
         res.status(300).send(JSON.stringify("ERROR: a valid owner id must be specified"))
         return
